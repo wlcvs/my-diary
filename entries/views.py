1bcfa840578ef198d437918890e6ed078bc4d412
Wallacy V. <wlcsv8080@outlook.com>
2024-12-11 19:47:22 -0300

    Escrever css por aqui é um inferno

---
+++ entries/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import (
+    ListView,
+    DetailView,
+)
+
+from .models import Entry
+
+class EntryListView(ListView):
+    model = Entry
+    queryset = Entry.objects.all().order_by('-date_created')
+
+class EntryDetailView(DetailView):
+    model = Entry
