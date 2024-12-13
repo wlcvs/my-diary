d4ce1fcffde9c19b5b23269fcf9db20de298821b
Wallacy V. <wlcsv8080@outlook.com>
2024-12-13 19:09:35 -0300

    Mais uma referência

--- entries/views.py
+++ entries/views.py
@@ -1,13 +1,45 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
 )
 
 from .models import Entry
 
-class EntryListView(ListView):
+class LockedView(LoginRequiredMixin):
+    login_url = "admin:login"
+
+class EntryListView(LockedView, ListView):
     model = Entry
     queryset = Entry.objects.all().order_by('-date_created')
 
-class EntryDetailView(DetailView):
+class EntryDetailView(LockedView, DetailView):
     model = Entry
+
+class EntryCreateView(LockedView, SuccessMessageMixin, CreateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_url = reverse_lazy("entry-list")
+    success_message = "Your new entry was created!"
+
+class EntryUpdateView(LockedView, SuccessMessageMixin, UpdateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_message = "Your entry has updated!"
+
+    def get_success_url(self):
+        return reverse_lazy(
+            "entry-detail",
+            kwargs = {"pk": self.object.id}
+        )
+
+class EntryDeleteView(LockedView, SuccessMessageMixin, DeleteView):
+    model = Entry
+    success_url = reverse_lazy("entry-list")
+    success_message = "Your entry was deleted!"
